643e970cf260a6710843492f89d30aec7150d276
Maxime19rd <117834727+Maxime19rd@users.noreply.github.com>
2025-05-20 11:18:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const projects = [
-  {
-    title: "Classification supervisée",
-    description: "Méthodes de classification en R et Python.",
-    image: "images/classification.png",
-    date: "Mars 2024",
-    url: "https://cel.hal.science/hal-04690630v2"
-  },
-  {
-    title: "App R Shiny ML",
+    {
+    title: "Application R Shiny ML",
     description: "Application interactive en R Shiny pour tester des modèles de machine learning.",
     image: "images/shinyapp.png",
-    date: "Mai 2024",
+    date: "Février 2025",
     url: "https://qnr4y4-maxime-rey0dor0ne.shinyapps.io/application/"
+  },
+  {
+    title: "Méthodes de classification supervisée en R et Python",
+    description: "Méthodes de classification en R et Python.",
+    image: "images/classification.png",
+    date: "Juillet 2024",
+    url: "https://cel.hal.science/hal-04690630v2"
   }
 ];
 
@@ -20,13 +20,16 @@ const container = document.getElementById("project-list");
 projects.forEach(project => {
   const div = document.createElement("div");
   div.className = "project";
-  div.innerHTML = `
-    <img src="${project.image}" alt="${project.title}" class="project-img" />
+div.innerHTML = `
+  <img src="${project.image}" alt="aperçu du projet" class="project-img">
+  <div class="project-content">
     <h3>${project.title}</h3>
-    <p><em>${project.date}</em></p>
+    <p><strong>Date :</strong> ${project.date}</p>
     <p>${project.description}</p>
     <a href="project.html?url=${encodeURIComponent(project.url)}&title=${encodeURIComponent(project.title)}">Voir le projet →</a>
-  `;
+  </div>
+`;  
+
   container.appendChild(div);
 });
 
